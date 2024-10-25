4582d708f311afd1978d45056e5dbfdfe50ae813
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2024-10-25 16:19:05 -0500

    estilo

--- src/screens/dashboard.ts
+++ src/screens/dashboard.ts
@@ -82,6 +82,7 @@ class Dashboard extends HTMLElement {
                         <p>Álbum</p>
                         <p id="fecha">Fecha en que se publicó</p>
                         <div id="svg">
+                            <p>Duración</p>
                             <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#ccccccd8" d="M12 20c4.42 0 8-3.58 8-8s-3.58-8-8-8s-8 3.58-8 8s3.58 8 8 8m0-18c5.5 0 10 4.5 10 10s-4.5 10-10 10C6.47 22 2 17.5 2 12S6.5 2 12 2m.5 11H11V7h1.5v4.26l3.7-2.13l.75 1.3z"/>
                             </svg>
                         </div>
